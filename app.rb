f4c43c6cada2ea607cdb736ab4b7e1c5f52fc7a5
Hans Lemuet <hans@etaminstudio.com>
2015-11-18 17:28:34 +0100

    added token check

--- app.rb
+++ app.rb
@@ -2,18 +2,38 @@ require 'rubygems'
 require 'sinatra'
 require 'http'
 
+helpers do
+  def valid_token?
+    return params['token'] === ENV['NATIONBUILDER_WEBHOOK_TOKEN']
+  end
+
+  def check_token
+    unless valid_token?
+      halt 403, 'Bad token'
+    end
+  end
+end
+
 get '/' do
   "Hello from Sinatra on Heroku!"
 end
 
 post '/people/created' do
+  check_token
   'People created'
 end
 
 post '/people/changed' do
+  check_token
   'People changed'
 end
 
+post '/people/merged' do
+  check_token
+  'People merged'
+end
+
 post '/people/deleted' do
+  check_token
   'People deleted'
 end
